d9bf022b579ec0b7996d71f8181dc3eb6b7d3e72
Kristina1303 <82513090+Kristina1303@users.noreply.github.com>
2021-06-09 11:57:10 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int char adjGirl[11] = { "beautiful", "crazy", "elegant", "slim", "pretty", "smart", "funny", "interesting", "dangerous", "sweet", "amaizing" };
+	int char adjBoys[11] = { "strong", "generous", "funny", "handsome", "polite", "kind", "evil", "sporty", "charming", "fit","confident" };
+	int char nounGirls[11] = { "princess", "queen", "model", "Party girl", "Lady", "royalty", "Superstar", "blonde", "actress", ""};
+	int char nounBoys[11] = { "king", "hero", "villain", "genius", "champion", "player", "boss", "prince", "captan", "God", "president" };
+
+
+
+}
